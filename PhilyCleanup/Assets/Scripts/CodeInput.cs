ca8b25eb12d9bf3db058375df064d3763e2ddb22
Michael Needleman <unconfigured@null.spigotmc.org>
2019-05-04 15:34:04 -0400

    Made a text input and button

---
+++ PhilyCleanup/Assets/Scripts/CodeInput.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CodeInput : MonoBehaviour
+{
+	public Button button;
+	public InputField inputField;
+
+	public string ToEdit = "Hello World";
+	public int x = 10;
+	public int y = 10;
+
+	void OnGUI()
+	{
+		
+	}
+	// Start is called before the first frame update
+	void Start()
+    {
+		button.onClick.AddListener(HandleButtonClick);    
+    }
+
+	public void HandleButtonClick()
+	{
+		print(inputField.text);
+	}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
