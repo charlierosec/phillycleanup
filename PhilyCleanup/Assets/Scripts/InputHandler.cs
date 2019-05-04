7658ab196d5282195b6191a029bbfea652180680
Michael Needleman <unconfigured@null.spigotmc.org>
2019-05-04 13:53:03 -0400

    Basic InputHandler WIP

---
+++ PhilyCleanup/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+	public bool isMiniGame;
+	public delegate void TestEvent();
+	public static event TestEvent OnEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			if(OnEvent != null)
+			{
+				OnEvent();
+			}
+		}	
+    }
+}
